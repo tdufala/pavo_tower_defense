b0764c93900614aaa8835675083e37ac3b192d78
Tim Dufala <dufalat@oregonstate.edu>
2018-07-18 22:37:08 -0700

    Adding assets. Original assets are in assets/ui/kenney. Moved contents of assets/ui/kenney/images to assets/images for use. Modified game.js to make each scene a new class which extends Phaser.Scene. Added Start menu scene, and buttons to that scene and phaser scene for UI dev.

--- src/game.js
+++ src/game.js
@@ -1,38 +1,85 @@
+// ======== Imports ========
 import 'phaser';
 import css from './stylesheets/main.css'
-var scenes = [];
-var level1Scene = new Phaser.Scene('Level_1');
-scenes.push(level1Scene);
 
+
+// ======== Globals ========
+
+var scenes = [];
 var config = {
     type: Phaser.AUTO,
     parent: 'phaser-app',
     width: 1344,
     height: 896,
-    pixelArt: true,
+    pixelArt: true, // antialias: false, roundPixels: true
     scene: scenes
 };
 
-var game = new Phaser.Game(config);
+// ======== Scenes ========
 
-//updated to facilitate separating different scenes corresponding to different levels/menu in different files
+// Start Menu (first scene)
+var StartMenuScene = new Phaser.Class({
+    Extends: Phaser.Scene,
 
-level1Scene.preload = function (){
+    initialize: function StartMenuScene() {
+        Phaser.Scene.call(this, { key: 'startMenu'});
+    },
 
-    this.load.image('gameTiles', 'assets/spritesheets/towerDefense_tilesheet.png');
-    this.load.tilemapTiledJSON('level1', 'assets/maps/level1.json');
+    preload: function() {
+        this.load.image('blueButton', 'assets/images/blue_button09.png');
+    },
 
-}
+    create: function() {
+        this.add.text(75, 170, 'Start Menu', { fontSize: '100px', color:'#0000FF' });
+        
+        // Start Button
+        var btnStart = this.add.sprite(790, 160, 'blueButton').setInteractive();
+        btnStart.setDisplaySize(32,32);
+        //btnStart.on('pointerover', function (event) { btnStart.setTexture('imgButtonStartHover');/* Do something when the mouse enters */ });
+        //btnStart.on('pointerout', function (event) { btnStart.setTexture('imgButtonStartNormal');/* Do something when the mouse exits. */ });
+        btnStart.on('pointerdown', function(event) {
+            this.scene.start('level1');
+        }, this); // Start the main game.
+        
+    }
+});
 
-level1Scene.create = function (){
- 
-    this.map = this.add.tilemap('level1');
-    var tiles = this.map.addTilesetImage('tileset', 'gameTiles');
+
+// Level 1
+var Level1Scene = new Phaser.Class({
+    Extends: Phaser.Scene,
     
-    this.backgroundLayer = this.map.createStaticLayer('background', tiles);
-    this.rockLayer = this.map.createStaticLayer('rocks', tiles);
-	
-}
+    initialize: function Level1Scene() {
+        Phaser.Scene.call(this, { key: 'level1'});
+    },
 
+    preload: function() {
+        this.load.image('gameTiles', 'assets/spritesheets/towerDefense_tilesheet.png');
+        this.load.tilemapTiledJSON('level1', 'assets/maps/level1.json');
+    },
 
+    create: function() {
+        this.map = this.add.tilemap('level1');
+        var tiles = this.map.addTilesetImage('tileset', 'gameTiles');
+        
+        this.backgroundLayer = this.map.createStaticLayer('background', tiles);
+        this.rockLayer = this.map.createStaticLayer('rocks', tiles);
 
+        // Back to start menu button
+        var btnStart = this.add.sprite(790, 160, 'blueButton').setInteractive();
+        btnStart.setDisplaySize(32,32);
+        //btnStart.on('pointerover', function (event) { btnStart.setTexture('imgButtonStartHover');/* Do something when the mouse enters */ });
+        //btnStart.on('pointerout', function (event) { btnStart.setTexture('imgButtonStartNormal');/* Do something when the mouse exits. */ });
+        btnStart.on('pointerdown', function(event) {
+            this.scene.start('startMenu');
+        }, this); // Return to the start menu.
+    }
+});
+
+// ======== Game start ========
+
+// Add Scenes
+scenes.push(StartMenuScene); // first scene pushed first
+scenes.push(Level1Scene);
+// Finally, start the game.
+var game = new Phaser.Game(config);
