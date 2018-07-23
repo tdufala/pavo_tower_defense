3a4900fbe4ab9b8b23af15e779aee16022206904
Tim Dufala <dufalat@oregonstate.edu>
2018-07-22 23:07:48 -0700

    Refactored using ES6 class expressions: - Created LevelScene class for levels, and pulled common code in. - Added some more parameters to LevelScene. - Updated the test path to use tileSize, giving a clean 'center of the road' path, following integer multiples of tileSize.

--- src/game.js
+++ src/game.js
@@ -34,36 +34,30 @@ function windowOnLoad() {
 
 
 // Start Menu (first scene)
-var StartMenuScene = new Phaser.Class({
-    Extends: Phaser.Scene,
+var StartMenuScene = class extends Phaser.Scene {
+    constructor() {
+        super('startMenu');
+    }
 
-    initialize: function StartMenuScene() {
-        Phaser.Scene.call(this, { key: 'startMenu'});
-
-    },
-
-    preload: function() {
+    preload() {
         this.load.image('blueButton', 'assets/images/blue_button09.png');
-    },
+    }
 
-    create: function() {
+    create() {
         var startMenuText = this.add.text(75, 75, 'Start Menu', { fontSize: '100px', color:'#0000FF'});
         var level1Text = this.add.text(75, 200, 'Level 1', { fontSize: '50px', color:'#00FF00' });
         var level2Text = this.add.text(75, 300, 'Level 2', { fontSize: '50px', color:'#00FF00' });
         var level3Text = this.add.text(75, 400, 'Level 3', { fontSize: '50px', color:'#00FF00' });
         // Start Button
-        var btnStart = this.add.sprite(this.sys.canvas.width - 125, 225, 'blueButton').setInteractive();
-        btnStart.setDisplaySize(100,100);
+        var lvl1Start = this.add.sprite(this.sys.canvas.width - 125, 225, 'blueButton').setInteractive();
 
         var lvl2Start = this.add.sprite(this.sys.canvas.width - 125, 325, 'blueButton').setInteractive();
-        btnStart.setDisplaySize(100,100);
 
         var lvl3Start = this.add.sprite(this.sys.canvas.width - 125, 425, 'blueButton').setInteractive();
-        btnStart.setDisplaySize(100,100);
         // following could be useful later:
-        //btnStart.on('pointerover', function (event) { btnStart.setTexture('imgButtonStartHover');/* Do something when the mouse enters */ });
-        //btnStart.on('pointerout', function (event) { btnStart.setTexture('imgButtonStartNormal');/* Do something when the mouse exits. */ });
-        btnStart.on('pointerdown', function(event) {
+        //lvl1Start.on('pointerover', function (event) { lvl1Start.setTexture('imgButtonStartHover');/* Do something when the mouse enters */ });
+        //lvl1Start.on('pointerout', function (event) { lvl1Start.setTexture('imgButtonStartNormal');/* Do something when the mouse exits. */ });
+        lvl1Start.on('pointerdown', function(event) {
             this.scene.start('level1');
         }, this); // Start the main game.
 
@@ -76,43 +70,34 @@ var StartMenuScene = new Phaser.Class({
         }, this); // Start the main game.
         
     }
-});
+};
 
 
-// Level 1
-var Level1Scene = new Phaser.Class({
-    Extends: Phaser.Scene,
-    
-    initialize: function Level1Scene() {
-        Phaser.Scene.call(this, { key: 'level1'});
-    },
-
-    preload: function() {
-		//load game map
-        this.load.image('gameTiles', 'assets/spritesheets/towerDefense_tilesheet.png');
-        this.load.tilemapTiledJSON('level1', 'assets/maps/level1.json');
-				//constants used in this game level and/or game
+// Base class for levels
+class LevelScene extends Phaser.Scene {
+    constructor(str) {
+        super(str);
+        this.levelName = str;
 		this.map_width = 1344;
-		this.ground_enemy_start = {'x': -50, 'y': 576};
-		this.ground_enemy_stop = {'x': this.map_width + 50, 'y': 288};
+        this.map_height = 896;
+        this.tileSize = { 'x': 32, 'y': 32 };
 
-		this.air_enemy_start = {'x' : -50, 'y': -50};
-		this.air_enemy_stop = {'x': 1390, 'y': 900/*ish*/};
+		this.ground_enemy_start = { 'x': 0, 'y': 0 };
+		this.ground_enemy_stop  = { 'x': 0, 'y': 0 };
+		this.air_enemy_start    = this.ground_enemy_start;
+		this.air_enemy_stop     = this.ground_enemy_stop;
+    }
 
-		
+    preload() {
+        // Load common assets
+        this.load.image('blueButton', 'assets/images/blue_button09.png');
+        this.load.image('gameTiles', 'assets/spritesheets/towerDefense_tilesheet.png');
 		this.load.spritesheet('enemy_sprite', 'assets/spritesheets/towerDefense_tilesheet.png', { frameWidth: 64, frameHeight: 64} );
-    },
+        this.load.tilemapTiledJSON(this.levelName, 'assets/maps/' + this.levelName + '.json');
+    }
 
-    create: function() {
-		//create tilemap and set tileset
-        this.map = this.add.tilemap('level1');
-        var tiles = this.map.addTilesetImage('tileset', 'gameTiles');
-        
-		//set map layers
-        this.backgroundLayer = this.map.createStaticLayer('background', tiles);
-        this.rockLayer = this.map.createStaticLayer('rocks', tiles);
-	
-		//add UI
+    create() {
+        // ---- UI elements ----
         var startMenuText = this.add.text(this.sys.canvas.width - 300, this.sys.canvas.height - 100, 'Return to Menu', { fontSize: '50px', color:'#00FF00', rtl: true});
 
         // Back to start menu button
@@ -121,6 +106,71 @@ var Level1Scene = new Phaser.Class({
         btnStart.on('pointerdown', function(event) {
             this.scene.start('startMenu');
         }, this); // Return to the start menu.
+ 
+		// ---- Tilemap ----
+        this.map = this.add.tilemap(this.levelName);
+        var tiles = this.map.addTilesetImage('tileset', 'gameTiles');
+		// Set map layers
+        this.backgroundLayer = this.map.createStaticLayer('background', tiles);
+        this.rockLayer = this.map.createStaticLayer('rocks', tiles);
+	    
+        // ---- Graphics ----
+        this.graphics = this.add.graphics();
+	    this.path = new Phaser.Curves.Path();
+ 	    //this.line1 = new Phaser.Curves.Line([ this.ground_enemy_start.x, this.ground_enemy_start.y, 288, 576 ]);
+ 	    this.line1 = new Phaser.Curves.Line([ this.ground_enemy_start.x, this.ground_enemy_start.y, this.ground_enemy_start.x, this.ground_enemy_start.y ]);
+	    this.path.add(this.line1);
+
+	    // ---- Units ----
+        this.followers = this.add.group();
+    }
+
+	update() {
+		
+		this.graphics.clear();
+
+		this.graphics.lineStyle(0, 0, 0);
+
+		this.path.draw(this.graphics);
+
+		var enemies = this.followers.getChildren();
+
+		for (var i = 0; i < enemies.length; i++) {
+		    //depending on type of enemy follow path or go straight to ending position for air units
+		    var t = enemies[i].z;
+		    var vec = enemies[i].getData('vector');
+
+		    //  The vector is updated in-place
+		    this.path.getPoint(t, vec);
+		    
+		    enemies[i].setPosition(vec.x, vec.y);
+
+		    enemies[i].setDepth(enemies[i].y);
+		}
+	} 
+
+};
+// Level 1
+var Level1Scene = class extends LevelScene {
+    constructor() {
+        super('level1');
+		// Constants specific to this level
+		this.ground_enemy_start = {'x': -2 * this.tileSize.x,
+                                   'y': 18 * this.tileSize.y};
+		this.ground_enemy_stop  = {'x': this.map_width + 2 * this.tileSize.x,
+                                   'y': 10 * this.tileSize.y};
+
+		this.air_enemy_start    = this.ground_enemy_start;
+		this.air_enemy_stop     = this.ground_enemy_stop;
+    }
+
+    preload() {
+        super.preload();
+    }
+
+    create() {
+        super.create();
+	
 		
 		//create towers
 		
@@ -133,146 +183,79 @@ var Level1Scene = new Phaser.Class({
 		//set path as lines
 				//this.load.image('enemy_sprite', 32, 576, 'sprites', 245);
 		//this.add.sprite(32, 576, 'enemy_sprite', 245);
-	this.graphics = this.add.graphics();
-		
- 	var line1 = new Phaser.Curves.Line([ this.ground_enemy_start.x, this.ground_enemy_start.y, 288, 576 ]);
-	this.path = new Phaser.Curves.Path();
-	this.path.add(line1);
-	this.path.lineTo(288, 160);
-	this.path.lineTo(672, 160);
-	this.path.lineTo(672, 768);
-	this.path.lineTo(1088, 768);
-	this.path.lineTo(1088, 288);
-	this.path.lineTo(this.ground_enemy_stop.x, this.ground_enemy_stop.y);
-	/*
-	//then add points to go to
-    path.lineTo(500, 400);
+	    	
+	    this.path.lineTo(9 * this.tileSize.x, this.ground_enemy_start.y);
+	    this.path.lineTo(9 * this.tileSize.x, 6 * this.tileSize.y);
+	    this.path.lineTo(21 * this.tileSize.x, 6 * this.tileSize.y);
+	    this.path.lineTo(21 * this.tileSize.x, 24 * this.tileSize.y);
+	    this.path.lineTo(34 * this.tileSize.x, 24 * this.tileSize.y);
+	    this.path.lineTo(34 * this.tileSize.x, 10 * this.tileSize.y);
+	    this.path.lineTo(this.ground_enemy_stop.x, this.ground_enemy_stop.y);
+	    //then add points to go to
 
-    //  Create the path followers
 
-	//create a follower group*/
-    this.followers = this.add.group();
 
-    for (var i = 0; i < 10; i++)
-    {
-		//creates a game object - read documentation for how to create object attributes : https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Group.html#create__anchor
-		//Once array of enemies containing wave data is created above, use this to iterate through for loop setting sprite data and starting position
-		//if certain type create of type i.e. enemy-sprite
-        var enemy_sprite = this.followers.create(this.ground_enemy_start.x,this.ground_enemy_start.y, 'enemy_sprite', 245);
-		//else do something with air units
-		
-		
-		//game object functions: https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.GameObject.html#setData__anchor
-        enemy_sprite.setData('vector', new Phaser.Math.Vector2());//, 'hp': 100, 'type': 'someobject defining motion', 'setmoredatahere': 0});
-	
-		var velocity = 100 //pixels/sec
-		
-		//define animation
-        this.tweens.add({
-		//can use conditional in update to change path if unit is flying
-            targets: enemy_sprite,
-            z: 1,
-            ease: 'Linear',
-			//duration is in ms, determine based on velocity in pixels/sec over mapsize
-           // duration: Math.floor(MAP_WIDTH/enemy_sprite.getData('velocity')) * 1000,
-		   duration: (Math.floor(this.map_width/velocity) * 500 * Math.random()),
-            repeat: 0,
-            delay: 200*i
-        });
-	}
-	
-}
-,
-	update: function(){
-		
-		this.graphics.clear();
-
-		this.graphics.lineStyle(0, 0, 0);
-
-		this.path.draw(this.graphics);
-
-		var enemies = this.followers.getChildren();
-
-		for (var i = 0; i < enemies.length; i++){
-				//depending on type of enemy follow path or go straight to ending position for air units
-				var t = enemies[i].z;
-				var vec = enemies[i].getData('vector');
-
-				//  The vector is updated in-place
-				this.path.getPoint(t, vec);
-				
-				enemies[i].setPosition(vec.x, vec.y);
-
-				enemies[i].setDepth(enemies[i].y);
-			}
-	} 
-
-});
+        for (var i = 0; i < 10; i++) {
+	    	//creates a game object - read documentation for how to create object attributes : https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Group.html#create__anchor
+	    	//Once array of enemies containing wave data is created above, use this to iterate through for loop setting sprite data and starting position
+	    	//if certain type create of type i.e. enemy-sprite
+            var enemy_sprite = this.followers.create(this.ground_enemy_start.x,this.ground_enemy_start.y, 'enemy_sprite', 245);
+	    	//else do something with air units
+	    	
+	    	
+	    	//game object functions: https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.GameObject.html#setData__anchor
+            enemy_sprite.setData('vector', new Phaser.Math.Vector2());//, 'hp': 100, 'type': 'someobject defining motion', 'setmoredatahere': 0});
+	    
+	    	var velocity = 100 //pixels/sec
+	    	
+	    	//define animation
+            this.tweens.add({
+	    	//can use conditional in update to change path if unit is flying
+                targets: enemy_sprite,
+                z: 1,
+                ease: 'Linear',
+	    		//duration is in ms, determine based on velocity in pixels/sec over mapsize
+               // duration: Math.floor(MAP_WIDTH/enemy_sprite.getData('velocity')) * 1000,
+	    	   duration: (Math.floor(this.map_width/velocity) * 500 * Math.random()),
+                repeat: 0,
+                delay: 200*i
+            });
+	    }
+    }
+};
 
 
 
 
 // Level 2
-var Level2Scene = new Phaser.Class({
-    Extends: Phaser.Scene,
-    
-    initialize: function Level2Scene() {
-        Phaser.Scene.call(this, { key: 'level2'});
-    },
-
-    preload: function() {
-        this.load.image('gameTiles', 'assets/spritesheets/towerDefense_tilesheet.png');
-        this.load.tilemapTiledJSON('level2', 'assets/maps/level2.json');
-    },
-
-    create: function() {
-        this.map = this.add.tilemap('level2');
-        var tiles = this.map.addTilesetImage('tileset', 'gameTiles');
-        
-        this.backgroundLayer = this.map.createStaticLayer('background', tiles);
-        this.rockLayer = this.map.createStaticLayer('rocks', tiles);
-
-        var startMenuText = this.add.text(this.sys.canvas.width - 300, this.sys.canvas.height - 100, 'Return to Menu', { fontSize: '50px', color:'#00FF00', rtl: true});
-        
-        // Back to start menu button
-        var btnStart = this.add.sprite(this.sys.canvas.width - 100, this.sys.canvas.height - 100, 'blueButton').setInteractive();
-        btnStart.setDisplaySize(100,100);
-        btnStart.on('pointerdown', function(event) {
-            this.scene.start('startMenu');
-        }, this); // Return to the start menu.
+var Level2Scene = class extends LevelScene {
+    constructor() {
+        super('level2');
     }
-});
+
+    preload() {
+        super.preload();
+    }
+
+    create() {
+        super.create();
+    }
+};
 
 // Level 3
-var Level3Scene = new Phaser.Class({
-    Extends: Phaser.Scene,
-    
-    initialize: function Level3Scene() {
-        Phaser.Scene.call(this, { key: 'level3'});
-    },
-
-    preload: function() {
-        this.load.image('gameTiles', 'assets/spritesheets/towerDefense_tilesheet.png');
-        this.load.tilemapTiledJSON('level3', 'assets/maps/level3.json');
-    },
-
-    create: function() {
-        this.map = this.add.tilemap('level3');
-        var tiles = this.map.addTilesetImage('tileset', 'gameTiles');
-        
-        this.backgroundLayer = this.map.createStaticLayer('background', tiles);
-        this.rockLayer = this.map.createStaticLayer('rocks', tiles);
-
-        var startMenuText = this.add.text(this.sys.canvas.width - 300, this.sys.canvas.height - 100, 'Return to Menu', { fontSize: '50px', color:'#00FF00', rtl: true});
-
-        // Back to start menu button
-        var btnStart = this.add.sprite(this.sys.canvas.width - 100, this.sys.canvas.height - 100, 'blueButton').setInteractive();
-        btnStart.setDisplaySize(100,100);
-        btnStart.on('pointerdown', function(event) {
-            this.scene.start('startMenu');
-        }, this); // Return to the start menu.
+var Level3Scene = class extends LevelScene {
+    constructor() {
+        super('level3');
     }
-});
+
+    preload() {
+        super.preload();
+    }
+
+    create() {
+        super.create();
+    }
+};
 
 // ======== Global Event Listeners ========
 window.addEventListener('load', windowOnLoad(), false);
