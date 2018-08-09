93736f4e115c672e6e818006cafcd27d0808be4a
jigar-gor <gorjigaru@gmail.com>
2018-08-09 12:23:57 -0700

    basic projectiles working

--- src/towers/projectile.js
+++
@@ -1,7 +0,0 @@
-/********************************************************************************
-* Projectile source code (projectile.js)
-* Description: contains source code for all projectile logic: loading, movement
-* etc.
-*
-*
-**********************************************************************************/
